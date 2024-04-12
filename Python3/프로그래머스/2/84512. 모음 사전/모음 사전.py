045029758a78058979d81dca5318a341118d5734
Dayeong Kim <ekdud_____@naver.com>
2024-04-13 02:55:03 +0900

    [level 2] Title: 모음 사전, Time: 1.30 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ Python3/프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,3 +1,11 @@
+from itertools import product
+
 def solution(word):
-    answer = 0
-    return answer
+    dict = []
+    
+    for i in range(1, 6):
+        for l in product(['A','E','I','O','U'], repeat = i):
+            dict.append(''.join(list(l)))
+    dict.sort()
+    
+    return dict.index(word) +1
